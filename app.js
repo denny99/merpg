08df2c3fa753c7a061f7cd18f260fbebb0463ca6
denny99 <denny@mvh-ag.de>
2014-07-28 19:34:31 +0200

    server

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+//start app
 var app = express();
+
+//program config
+global.config = require('./server/settings/config.json')[app.get('env')];
+
+var routes = require('./routes/index');
+var api = require('./routes/api_v1.0');
+
+//program params
+console.log("Application Environment: " + app.get('env'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/', api);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api_v1.0.js
+++ routes/api_v1.0.js
@@ -5,6 +5,68 @@
 var express = require('express');
 var router = express.Router();
 
-/* set api routes. */
+var apiConfig = require('../server/js/API');
+
+/* set apiConfig.API routes. */
+
+router.get('/api_v1.0/get/table/:name', function(req, res) {
+    apiConfig.API.getTable(req.params.name, function (table) {
+        res.send(table);
+    });
+});
+
+router.get('/api_v1.0/get/monster/:name', function(req, res) {
+    apiConfig.API.getMonster(req.params.name, function (monster) {
+        res.send(monster);
+    });
+});
+
+router.get('/api_v1.0/get/character/:playerID/:name', function(req, res) {
+    apiConfig.API.getCharacter(req.params.name, req.params.playerID, function (character) {
+        res.send(character);
+    });
+});
+
+router.get('/api_v1.0/get/item/:name', function(req, res) {
+    apiConfig.API.getItem(req.params.name, function (item) {
+        res.send(item);
+    });
+});
+
+router.get('/api_v1.0/delete/monster/:name', function(req, res) {
+    apiConfig.API.getMonster(req.params.name, function (monster) {
+        res.send(monster);
+    });
+});
+
+router.get('/api_v1.0/delete/character/:playerID/:name', function(req, res) {
+    apiConfig.API.getCharacter(req.params.name, req.params.playerID, function (character) {
+        res.send(character);
+    });
+});
+
+router.get('/api_v1.0/delete/item/:name', function(req, res) {
+    apiConfig.API.getItem(req.params.name, function (item) {
+        res.send(item);
+    });
+});
+
+router.get('/api_v1.0/insert/item', function(req, res) {
+    apiConfig.API.insertItem(JSON.parse(req.params.json), function (err) {
+        res.send(err);
+    });
+});
+
+router.get('/api_v1.0/insert/monster', function(req, res) {
+    apiConfig.API.insertMonster(JSON.parse(req.params.json), function (err) {
+        res.send(err);
+    });
+});
+
+router.get('/api_v1.0/insert/character', function(req, res) {
+    apiConfig.API.insertCharacter(JSON.parse(req.params.json), function (err) {
+        res.send(err);
+    });
+});
 
 module.exports = router;
