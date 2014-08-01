e5d8fdf1d990795715ca1a6bea8668ee67b2c080
denny99 <denny@mvh-ag.de>
2014-08-01 19:22:08 +0200

    google login

--- routes/routes.js
+++ routes/routes.js
@@ -5,14 +5,19 @@ var login = require('../server/js/login');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('layout', { title: 'MERS BattleApp' });
+  res.render('layout', { title: 'MERS BattleApp', loginStatus: req.isAuthenticated()});
 });
 
-router.get('/login/facebook', function (req, res, next) {
-    return login.facebook(req, res, next);
+router.get('/login/google', function (req, res, next) {
+    return login.google(req, res, next);
 });
-router.get('/login/facebook/callback', function (req, res, next) {
-    login.facebookcb(req, res, next);
+router.get('/login/google/callback', function (req, res, next) {
+    login.googlecb(req, res, next);
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
--- server/js/login.js
+++ server/js/login.js
@@ -1,34 +1,24 @@
 var passport = require('passport');
-var FacebookStrategy = require('passport-facebook').Strategy;
-
-var FACEBOOK_APP_ID = "1407742842797793";
-var FACEBOOK_APP_SECRET = "aa868858e4d8b661710c8029e25b5f86";
 
 var settings = require('../settings/settings');
 var users = settings.getDB("users");
 
-// facebook Strategie
-passport.use(new FacebookStrategy({
-        clientID: FACEBOOK_APP_ID,
-        clientSecret: FACEBOOK_APP_SECRET,
-        callbackURL: "http://127.0.0.1:3000/login/facebook/callback/"
-    },
-    function (accessToken, refreshToken, profile, done) {
-        process.nextTick(function () {
-            // To keep the example simple, the user's Facebook profile is returned to
-            // represent the logged-in user.  In a typical application, you would want
-            // to associate the Facebook account with a user record in your database,
-            // and return that user instead.
-            return done(null, profile);
-        });
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+passport.use(new GoogleStrategy({
+        clientID: "335800780801-v5o7ahbg84ulgepb6ev3822l945bsjsj.apps.googleusercontent.com",
+        clientSecret: "XhZhfUp-E2kE_6HBvj7s5o43",
+        callbackURL: "http://127.0.0.1:3000/login/google/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+            return done(null, profile);
     }
 ));
 
 
-exports.facebook = passport.authenticate('facebook', {scope: 'email'});
+exports.google = passport.authenticate('google', {scope: 'email'});
 
-exports.facebookcb = passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/' });
+exports.googlecb = passport.authenticate('google', {successRedirect: '/', failureRedirect: '/' });
 
 //*** settings for passport module ***
 passport.serializeUser(function (user, done) {
