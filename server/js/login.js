d36b41dd4482e13fb8660834747f66d5d51b9e89
denny99 <denny@mvh-ag.de>
2014-08-04 16:13:06 +0200

    demo

--- server/js/login.js
+++ server/js/login.js
@@ -8,7 +8,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
         clientID: "335800780801-v5o7ahbg84ulgepb6ev3822l945bsjsj.apps.googleusercontent.com",
         clientSecret: "XhZhfUp-E2kE_6HBvj7s5o43",
-        callbackURL: "http://127.0.0.1:3000/login/google/callback"
+        callbackURL: "http://merpg.herokuapp.com/login/google/callback"
+        //callbackURL: "http://127.0.0.1:3000/login/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
             return done(null, profile);
