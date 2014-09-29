da109521d52a18b3822cc2e766ae017d094af024
denny99 <denny@mvh-ag.de>
2014-09-29 19:25:00 +0200

    random id

--- public/js/helper.js
+++ public/js/helper.js
@@ -161,3 +161,14 @@ ko.extenders.id = function(target) {
     //return the new computed observable
     return result;
 };
+
+function generateId()
+{
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for( var i=0; i < 5; i++ )
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	return text;
+}
