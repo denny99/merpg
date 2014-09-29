6e2f481e789170c1217edbc3b891447a0fe02d0a
denny99 <denny@mvh-ag.de>
2014-09-29 19:30:43 +0200

    random id

--- public/js/helper.js
+++ public/js/helper.js
@@ -165,7 +165,7 @@ ko.extenders.id = function(target) {
 function generateId()
 {
 	var text = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var possible = "0123456789";
 
 	for( var i=0; i < 5; i++ )
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
