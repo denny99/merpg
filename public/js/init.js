f74048909b1f39140b086713b4de739c0af98597
denny99 <denny@mvh-ag.de>
2014-10-02 11:51:59 +0200

    disable tab zoom

--- public/js/init.js
+++ public/js/init.js
@@ -2,23 +2,6 @@
  * Created by Denny on 31.07.2014.
  */
 
-(function($) {
-	$.fn.nodoubletapzoom = function() {
-		$(this).bind('touchstart', function preventZoom(e) {
-			var t2 = e.timeStamp
-				, t1 = $(this).data('lastTouch') || t2
-				, dt = t2 - t1
-				, fingers = e.originalEvent.touches.length;
-			$(this).data('lastTouch', t2);
-			if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-
-			e.preventDefault(); // double tap - prevent the zoom
-			// also synthesize click events we just swallowed up
-			$(this).trigger('click').trigger('click');
-		});
-	};
-})(jQuery);
-
 //data storages
 var damageTables = {};
 var criticalTables = {};
